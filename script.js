855fb8132c5f5670c6dc643afcfbe0f06f4be280
mahfuz-antor <antorcnj2014@gmail.com>
2023-08-13 16:56:12 +0600

    task 7 done

--- script.js
+++ script.js
@@ -84,5 +84,31 @@ console.log(MathUtility.multiply(2, 8));
 // ****** Task-06 End *******
 
 // ****** Task-7) javascript es6 class inheritance *******
+class Animal {
+  constructor(name, sound) {
+    this.name = name;
+    this.sound = sound;
+  }
+
+  makeSound() {
+    console.log(`${this.sound}`);
+  }
+}
+
+class Dog extends Animal {
+  fetch() {
+    console.log("Fetching the ball!");
+  }
+}
+
+const animal = new Animal("Lion", "Roar");
+
+animal.makeSound(); // Output: "Roar"
+
+const dog = new Dog("Buddy", "Woof");
+
+dog.makeSound(); // Output: "Woof"
+
+dog.fetch();
 
 // ****** Task-07 End *******
